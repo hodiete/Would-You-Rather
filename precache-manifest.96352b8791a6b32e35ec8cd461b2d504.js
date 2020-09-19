876266601c8560c912d3377e1d241602b757f13c
henry <henry.odiete@opin.ca>
2020-09-19 18:37:06 -0400

    Updates

---
+++ precache-manifest.96352b8791a6b32e35ec8cd461b2d504.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a42137b70ee80c32ffeeaf9fcfcdd5f2",
+    "url": "/hodiete21/Would-You-Rather/index.html"
+  },
+  {
+    "revision": "c0fd7a0098dc1d17bc0f",
+    "url": "/hodiete21/Would-You-Rather/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "c74113b2ad734881bdf5",
+    "url": "/hodiete21/Would-You-Rather/static/css/main.d15032a8.chunk.css"
+  },
+  {
+    "revision": "c0fd7a0098dc1d17bc0f",
+    "url": "/hodiete21/Would-You-Rather/static/js/2.6c6f5765.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/hodiete21/Would-You-Rather/static/js/2.6c6f5765.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c74113b2ad734881bdf5",
+    "url": "/hodiete21/Would-You-Rather/static/js/main.2a04f8ba.chunk.js"
+  },
+  {
+    "revision": "31d7ea3e3b90e01beea2",
+    "url": "/hodiete21/Would-You-Rather/static/js/runtime-main.c343fe2a.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/hodiete21/Would-You-Rather/precache-manifest.96352b8791a6b32e35ec8cd461b2d504.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hodiete21/Would-You-Rather/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
